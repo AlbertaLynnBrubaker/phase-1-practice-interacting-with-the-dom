c585808e6e0de7e24a66c9bb4dc6412686215bb6
Jonathon <jwbialas@gmail.com>
2022-06-30 10:25:27 -0500

    submit form

--- js/challenge.js
+++ js/challenge.js
@@ -18,8 +18,19 @@ countMinus.addEventListener('click', () => {
     count--;
     updateDisplay();
     });
-
-
+     
+function komment(){
+    const comment = document.getElementById('comment-form');
+    comment.addEventListener("submit", e => {
+        e.preventDefault();
+        const input = e.target["comment-input"].value;
+        const newReview = document.createElement('li');
+        newReview.textContent = `${input}`;
+        const newDiv = document.getElementById('review');
+        newDiv.appendChild(newReview);
+    })
+}
+komment();
 // for(i; i > 0; i++) {
     
 // }
